67067d7ff98b6c8d082e6a7e2d1b77e669c1ac33
ediney-magalhaes <edy.estatistica@gmail.com>
2026-08-26 10:54:30 -0400

    fix(fase3): corrige classificacao entrada/saida em int_movimentacoes_uti (INTERNACAO direto em UTI e ultima unidade antes da alta estavam sendo perdidas)

--- dbt_classificacao_analytics/models/intermediate/int_movimentacoes_uti.sql
+++ dbt_classificacao_analytics/models/intermediate/int_movimentacoes_uti.sql
@@ -1,50 +1,40 @@
-with movimentacoes as(
-    select *
+with movimentacoes as (
+    select
+        *,
+        case
+            when tipo in ('INTERNACAO', 'TRANSFER. DE') then 'entrada'
+            when tipo in ('TRANSFER. PARA', 'ALTA') then 'saida'
+        end as tipo_evento
     from {{ ref('stg_bronze__movimentacoes') }}
 ),
 
-unidades_ocupadas as(
-    select *
-    from movimentacoes
-    where tipo in ('INTERNACAO', 'TRANSFER. PARA')
-),
-
-com_movimentacao_anterior as(
+ordenado as (
     select
         *,
-        lag(data_hora_movimentacao) over(
+        lead(data_hora_movimentacao) over (
             partition by atendimento
-            order by data_hora_movimentacao
-        ) as data_hora_movimentacao_anterior
-    from unidades_ocupadas
+            order by
+                data_hora_movimentacao,
+                case when tipo_evento = 'saida' then 0 else 1 end
+        ) as data_hora_saida_unidade
+    from movimentacoes
 ),
 
-unidades_de_saida as(
+unidades_ocupadas as (
     select
         atendimento,
         hash_nm_paciente,
         tipo,
         unidade,
-        data_hora_movimentacao_anterior as data_hora_entrada,
-        data_hora_movimentacao as data_hora_saida,
-        timestamp_diff(
-            data_hora_movimentacao,
-            data_hora_movimentacao_anterior,
-            minute
-        ) as minutos_na_unidade,
-        round(timestamp_diff(
-            data_hora_movimentacao,
-            data_hora_movimentacao_anterior,
-            minute
-        ) / 60.0, 1) as horas_na_unidade,
-        round(timestamp_diff(
-            data_hora_movimentacao,
-            data_hora_movimentacao_anterior,
-            minute
-        ) / 1440.0, 1) as dias_na_unidade,
+        data_hora_movimentacao as data_hora_entrada,
+        data_hora_saida_unidade as data_hora_saida,
+        timestamp_diff(data_hora_saida_unidade, data_hora_movimentacao, minute) as minutos_na_unidade,
+        round(timestamp_diff(data_hora_saida_unidade, data_hora_movimentacao, minute) / 60.0, 1) as horas_na_unidade,
+        round(timestamp_diff(data_hora_saida_unidade, data_hora_movimentacao, minute) / 1440.0, 1) as dias_na_unidade,
         upper(unidade) like '%UTI%' as eh_uti,
         safra_mes
-    from com_movimentacao_anterior
+    from ordenado
+    where tipo_evento = 'entrada'
 )
 
-select * from unidades_de_saida
+select * from unidades_ocupadas
